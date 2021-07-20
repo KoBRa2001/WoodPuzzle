a6a6bf1d1ddc4673cb113f6cc1a81917866cd4f6
KoBRa2001 <kobra864@gmail.com>
2021-07-20 21:02:07 +0300

    first commit

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
+{
+    private Vector3 offset;
+    private CanvasGroup canvasGroup;
+    [SerializeField]
+    private Transform dragParent;
+    private Transform startParent;
+    private Vector3 startPosition;    
+    
+    private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData touch)
+    {
+        startParent = transform.parent;
+        transform.SetParent(dragParent);
+        startPosition = transform.position;
+        Debug.Log("OnBeginDrag");
+        offset = transform.position - (Vector3)touch.position;
+        offset.z = 0f;
+        canvasGroup.blocksRaycasts = false;        
+    }
+
+    public void OnDrag(PointerEventData touch)
+    {
+        //Debug.Log("OnDrag");
+        transform.position = (Vector3)touch.position + offset;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrop");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Debug.Log("OnEndDrag");
+        if (transform.parent == dragParent)
+        {
+            transform.SetParent(startParent);
+            transform.position = startPosition;
+        }
+        canvasGroup.blocksRaycasts = true;
+    }
+}
---
+++ Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    [SerializeField]
+    private Transform sceneParent;
+
+    public void OnDrop(PointerEventData item)
+    {        
+        //Debug.Log("OnDrop");
+        if (item.pointerDrag != null && transform.childCount == 0)
+        {            
+            item.pointerDrag.transform.SetParent(sceneParent);
+            item.pointerDrag.transform.position = transform.position;
+        }        
+    }
+}
